2e4951a8553ce25cba88284edd522edb18e9af8f
Sergi Zelen <greatdeveloperSerg@gmail.com>
2020-02-24 23:17:06 +0200

    initial commit

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -48,6 +48,8 @@ export class ExpenseListFilters extends React.Component {
           showClearDates={true}
           numberOfMonths={1}
           isOutsideRange={() => false}
+          startDateId="start"
+          endDateId="end"
         />
       </div>
     );
