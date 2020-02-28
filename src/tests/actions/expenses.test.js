343a0839c6dbaec759b764c0de296c9413e4684b
Sergi Zelen <greatdeveloperSerg@gmail.com>
2020-02-28 10:26:20 +0200

    Initial commit

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -52,7 +52,7 @@ test('should add expense to database and store', (done) => {
       }
     });
 
-    return database.ref(`expense/${actions[0].expense.id}`).once('value');
+    return database.ref(`expenses/${actions[0].expense.id}`).once('value');
   }).then((snapshot) => {
     expect(snapshot.val()).toEqual(expenseData);
     done();
@@ -78,7 +78,7 @@ test('should add expense with defaults to database and store', (done) => {
       }
     });
 
-    return database.ref(`expense/${actions[0].expense.id}`).once('value');
+    return database.ref(`expenses/${actions[0].expense.id}`).once('value');
   }).then((snapshot) => {
     expect(snapshot.val()).toEqual(expenseDefaults);
     done();
